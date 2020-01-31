404821357044f76faa7ebf632975557b2703c9ca
Jose Amesquita <joseamesquita11@gmail.com>
2020-01-31 12:51:34 -0800

    1. add pizza prototype method

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,10 @@ function Order() {
   this.pizzas = [],
     this.currentId = 0
 }
-
+Order.prototype.addPizza = function (pizza) {
+  this.currentId += 1;
+  this.pizzas.push(pizza);
+}
 $(document).ready(function () {
 
 });
