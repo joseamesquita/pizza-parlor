f8adaa59e0e9220d92c28a8f8f83a7bcb5cca1cd
Jose Amesquita <joseamesquita11@gmail.com>
2020-02-03 18:59:58 -0800

    1. remove unnecessary code

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,8 @@
 // Business Logic
 
-function Order() {
-  this.pizzas = [],
-    this.cost = [];
-}
 function Pizza(size, toppings) {
   this.size = size,
     this.toppings = toppings;
-}
-
-Order.prototype.addPizza = function (pizza) {
-  pizza.cost = pizza.toppingCost(pizza);
-  this.pizzas.push(pizza);
-}
-
-Order.prototype.deletePizza = function (id) {
-  for (var i = 0; i < this.pizzas.length; i++) {
-    if (this.pizzas[i]) {
-      if (this.pizzas[i].id === id) {
-        delete this.pizzas[i];
-        return true;
-      }
-    }
-  }
-  return false;
 }
 
 Pizza.prototype.totalCost = function (arr) {
@@ -47,18 +26,10 @@ $(document).ready(function () {
       input.push(toppingCosts);
     });
 
-    var inputToppings = [];
-    $("input:checkbox[name=topping]:checked").each(function () {
-      var top = $(this).val();
-      inputToppings.push(top);
-    });
-
     var sizes = parseInt($("#sizes").val());
-
     var newPizza = new Pizza(sizes, input);
-    console.log(newPizza);
     var pizzaCost = newPizza.totalCost(input);
-    console.log(pizzaCost);
+
 
     $("#pizza-cost").text(pizzaCost);
     $(".results").show();
